c873167cb412feac46b885165e7d4ca5abec5bb6
sayandip199309@gmail.com <sayandip199309@gmail.com>
2023-03-30 13:59:28 +0530

    feat: Add error handling

--- assistant.py
+++ assistant.py
@@ -1,4 +1,5 @@
 import os
+import time
 from typing import Literal, TypedDict
 
 import openai
@@ -30,35 +31,89 @@ class Assistant:
 
     @classmethod
     def new_question(cls, question: str):
-        message: Message = {'role': 'user', 'content': question}
+        message: Message = {"role": "user", "content": question}
         cls.conversation.append(message)
         # FIX: Handle API connection error
-        response = openai.ChatCompletion.create(
-            model=MODEL,
-            messages=cls.conversation,
-        )
+        response = cls.handle_response(cls.conversation)
+        if not response:
+            answer = "[red]No response received!!"
+            print(Panel(answer, title="Assistant", subtitle=f"0 tokens consumed"))
+            quit()
         reply, answer, tk = cls.process_response(response)
         cls.conversation.append(reply)
         print(Panel(answer, title="Assistant", subtitle=f"{tk} tokens consumed"))
 
     @staticmethod
+    def handle_response(history: list[Message], retry: int = 0):
+        # FIX: Add logging
+        try:
+            response = openai.ChatCompletion.create(
+                model=MODEL,
+                messages=history,
+            )
+        except openai.error.Timeout as e:
+            # Handle timeout error, e.g. retry or log
+            print(f"OpenAI API request timed out: {e}")
+            if retry < 5:
+                print(f"Retrying after 5 seconds.")
+                time.sleep(5)
+                retry += 1
+                Assistant.handle_response(history, retry)
+            return {}
+        except openai.error.APIError as e:
+            # Handle API error, e.g. retry or log
+            print(f"OpenAI API returned an API Error: {e}")
+            if retry < 5:
+                print(f"Retrying after 5 seconds.")
+                time.sleep(5)
+                retry += 1
+                Assistant.handle_response(history, retry)
+            return {}
+        except openai.error.APIConnectionError as e:
+            # Handle connection error, e.g. check network or log
+            print(f"OpenAI API request failed to connect: {e}")
+            return {}
+        except openai.error.InvalidRequestError as e:
+            # Handle invalid request error, e.g. validate parameters or log
+            print(f"OpenAI API request was invalid: {e}")
+            return {}
+        except openai.error.AuthenticationError as e:
+            # Handle authentication error, e.g. check credentials or log
+            print(f"OpenAI API request was not authorized: {e}")
+            return {}
+        except openai.error.PermissionError as e:
+            # Handle permission error, e.g. check scope or log
+            print(f"OpenAI API request was not permitted: {e}")
+            return {}
+        except openai.error.RateLimitError as e:
+            # Handle rate limit error, e.g. wait or log
+            print(f"OpenAI API request exceeded rate limit: {e}")
+            if retry < 5:
+                print(f"Retrying after 5 seconds.")
+                time.sleep(5)
+                retry += 1
+                Assistant.handle_response(history, retry)
+            return {}
+        return response
+
+    @staticmethod
     def process_response(response) -> tuple[Message, str, int]:
-        color = '[red]'
-        reply: Message = {'role': 'assistant', 'content': 'Sorry! Could not process response!!'}
+        color = "[red]"
+        reply: Message = {
+            "role": "assistant",
+            "content": "Sorry! Could not process response!!",
+        }
         total_tokens = response.usage.total_tokens
 
-        if response.choices[0].finish_reason == 'stop':
-            color = '[green]'
+        if response.choices[0].finish_reason == "stop":
+            color = "[green]"
             reply = response.choices[0].message
 
-        answer = color + reply['content']
+        answer = color + reply["content"]
 
         return reply, answer, total_tokens
 
 
-for user_input in iter(lambda: input("> "), ''):
+for user_input in iter(lambda: input("> "), ""):
     Assistant.new_session()
     Assistant.new_question(user_input)
-
-
-
